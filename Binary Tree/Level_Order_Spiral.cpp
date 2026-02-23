6833654daafe57070480f45e3cd7414007f5cad3
Raghav Shrimali <141941073+raghav-shrimali@users.noreply.github.com>
2026-02-23 22:01:36 +0530

    Create Level_Order_Spiral.cpp

---
+++ Binary Tree/Level_Order_Spiral.cpp
@@ -0,0 +1,36 @@
+/*
+Approach:
+- Use two stacks
+
+Time: O(N)
+Space: O(N)
+*/
+
+vector<int> findSpiral(Node* root) {
+    vector<int> ans;
+    if (!root) return ans;
+
+    stack<Node*> s1, s2;
+    s1.push(root);
+
+    while (!s1.empty() || !s2.empty()) {
+
+        while (!s1.empty()) {
+            Node* temp = s1.top(); s1.pop();
+            ans.push_back(temp->data);
+
+            if (temp->right) s2.push(temp->right);
+            if (temp->left) s2.push(temp->left);
+        }
+
+        while (!s2.empty()) {
+            Node* temp = s2.top(); s2.pop();
+            ans.push_back(temp->data);
+
+            if (temp->left) s1.push(temp->left);
+            if (temp->right) s1.push(temp->right);
+        }
+    }
+
+    return ans;
+}
